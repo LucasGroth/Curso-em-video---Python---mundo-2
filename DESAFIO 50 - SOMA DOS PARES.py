6c1490c5cd0f6355f7f54b166033357a3eb9d7a3
LucasGroth <lucaslwgo@gmail.com>
2026-03-24 11:24:18 -0300

    EXERCÍCIO 50 - SOMA DE NUMEROS PARES INFORMADOS

---
+++ DESAFIO 50 - SOMA DOS PARES.py
@@ -0,0 +1,4 @@
+#Um programa que leia seis numeros inteiros e some apenas os que forem pares.
+##PRIMEIRO DESAFIO - RECEBER OS NUMEROS INTEIROS E ARMAZENA-LOS PARA POSTERIOR ANALISE
+for c in range(0,6):
+    n=int(input('Digite um numero: '))
