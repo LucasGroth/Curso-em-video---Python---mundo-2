f28e69e80042c3364409b3fd79770e1199501aa4
LucasGroth <lucaslwgo@gmail.com>
2026-03-24 10:40:17 -0300

    EXERCÍCIO 46 - CONTAGEM REGRESSIVA - LAÇO FOR

---
+++ DESAFIO 46 - CONTAGEM REGRESSIVA.py
@@ -0,0 +1,6 @@
+#Um progrma que faça uma contagem regressiva para lançamento de fogos de artifícios, indo de 10 até 0 , com pausa de 1 segundo.
+import time
+for c in range(5,0, -1):
+    print(c)
+    time.sleep(1)
+print('Lançar!!!')
